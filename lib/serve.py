ed045ac06f746b4045e73e00b1c0f871f79f4055
Dudu Twizer <24310417+dudutwizer@users.noreply.github.com>
2022-04-07 03:55:43 +0300

    adding website

--- lib/serve.py
+++ lib/serve.py
@@ -1,16 +1,14 @@
 import boto3
+import os
 from ec2_metadata import ec2_metadata
-from flask import Flask, request, render_template
+from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder="build", template_folder="build")
+app = Flask(__name__, static_folder='react_app/build')
 
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-@app.route("/")
-def echo():
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
     instance_region = ec2_metadata.region
     client = boto3.client('ec2', region_name=instance_region)
     instance_id = ec2_metadata.instance_id
@@ -31,6 +29,9 @@ def echo():
     ]
 )
     response = "<h1>This is " + instance['Tags'][0]['Value'] + "</h1>"
-    return render_template('index.html', content=response)
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(use_reloader=True, host='0.0.0.0', port=80, threaded=True))
