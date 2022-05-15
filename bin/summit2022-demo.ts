a277506376f5bf0259ca111a20d29ad6a50e48c0
Dudu Twizer <dudut@amazon.com>
2022-05-15 14:06:56 +0300

    release

--- bin/summit2022-demo.ts
+++ bin/summit2022-demo.ts
@@ -15,5 +15,5 @@ new ShuffleShardingDemoSummit2022(app, 'ShuffleShardingDemoSummit2022', {
       shuffle: false,
     },
   },
-  props: { env: { account: '<changeme>', region: '<changeme>' } }, //Ireland
+  props: { env: { account: '<changeme>', region: '<changeme>' } },
 });
